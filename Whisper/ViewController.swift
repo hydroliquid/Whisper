2dee1f9adcf314b8340a2d9a11b3fed31edf31ed
School_Apple_PC <michael.j.benton@live.com>
2019-02-28 18:40:33 -0800

    Initial Commit

---
+++ Whisper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whisper
+//
+//  Created by MICHAEL BENTON on 2/28/19.
+//  Copyright © 2019 MICHAEL BENTON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
